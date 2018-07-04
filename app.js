109929a23346c99f249a70fedcd68f6d9ea9a0a4
carlacentenor <carlacentenor@gmail.com>
2018-07-04 16:34:02 -0500

    modificando json

--- app.js
+++ app.js
@@ -1,76 +1,74 @@
-const increment = $('#increment');
-const decrement = $('#decrement');
-let numberPizza = $('#americanagrande');
-let numberPizza2 = $('#pepperonigrande');
 let totalFinal = 0;
 localStorage.setItem('totalFinal', totalFinal);
 let showTotal = $('#total');
+const containerBig = $('.container-bigpizza');
+const containerFamily = $('.container-bigfamily');
+const containerAdicion = $('.container-adicional');
+// 
+$.getJSON('https://my-json-server.typicode.com/carlacentenor/webview/db', function (data) {
+  let pizzaBig = data.products.pizzas.grandes;
+  let pizzaFamily = data.products.pizzas.familiares;
+  let adicionales = data.products.adicionales;
+  pizzaBig.forEach(element => {
+    templateProducts(element, containerBig);
+  });
+  pizzaFamily.forEach(element => {
+    templateProducts(element,containerFamily);
+  });
+  adicionales.forEach(element => {
+    templateProducts(element, containerAdicion);
+  });
 
-$.getJSON('https://my-json-server.typicode.com/carlacentenor/webview/db', function(data) {
-   let pizzaBig = data.products.pizzas.grandes;
-  const container = $('.container-bigpizza');
-   pizzaBig.forEach(element => {
-      let template = ` <div class="row">
-      <div class="col-8">${element.nombre}</div>
-      <div class="col-4 row">
-        <div class="col-4">
-          <button id=${element.id} data-precio=${element.precio}>+</button>
-        </div>
-        <div class="col-4" id=${element.nombre}>0</div>
-        <div class="col-4">
-          <button id=${element.id2} data-precio=${element.precio}>-</button>
-        </div>
-      </div>
-    </div>`;
-    container.append(template);
-   });
-   
- });
+});
+
+//Eventos + / -
+$(document).on('click', '.increment', function () {
+  let price = $(this).data('precio');
+  let idNumber = $(this).context.parentElement.nextElementSibling.id;
+  incrementTotal(price, idNumber);
+})
+
+$(document).on('click', '.decrement', function () {
+  let price = $(this).data('precio');
+  let idNumber = $(this).context.parentElement.previousElementSibling.id;
+
+  decrementTotal(price, idNumber);
+})
 
 
-
-$(document).on('click', '#increment', function () {
-  let totalPedido =localStorage.getItem('totalFinal');
-  let number = numberPizza.text();
+// Funciones de Incrementar/Decrementar Precio
+let incrementTotal = (price, idNumberBox) => {
+  let totalPedido = localStorage.getItem('totalFinal');
+  let number = $(`#${idNumberBox}`).text();
   number = parseInt(number) + 1;
-  numberPizza.text(number); // mostrando valores 
-  let precio = $(this).data('precio');
-  let final =parseFloat(precio) + parseFloat(totalPedido);
+  $(`#${idNumberBox}`).text(number); // mostrando valores 
+  let final = parseFloat(price) + parseFloat(totalPedido);
   localStorage.setItem('totalFinal', final.toFixed(1));
   showTotal.text(localStorage.getItem('totalFinal'))
-});
+};
 
-$(document).on('click', '#decrement', function () {
-  let totalPedido =localStorage.getItem('totalFinal');
-  let number = numberPizza.text();
+let decrementTotal = (price, idNumberBox) => {
+  let totalPedido = localStorage.getItem('totalFinal');
+  let number = $(`#${idNumberBox}`).text();
   number = parseInt(number) - 1;
-  numberPizza.text(number); // mostrando valores 
-  let precio = $(this).data('precio');
-  let final = parseFloat(totalPedido)-parseFloat(precio);
+  $(`#${idNumberBox}`).text(number); // mostrando valores 
+  let final = parseFloat(totalPedido) - parseFloat(price);
   localStorage.setItem('totalFinal', final.toFixed(1));
   showTotal.text(localStorage.getItem('totalFinal'))
-});
+};
 
-$(document).on('click', '#increment1', function () {
-  let totalPedido =localStorage.getItem('totalFinal');
-  let number = numberPizza2.text();
-  number = parseInt(number) + 1;
-  numberPizza2.text(number); // mostrando valores 
-  let precio = $(this).data('precio');
-  let final =parseFloat(precio) + parseFloat(totalPedido);
-  localStorage.setItem('totalFinal', final.toFixed(1));
-  showTotal.text(localStorage.getItem('totalFinal'))
-});
-
-$(document).on('click', '#decrement2', function () {
-  let totalPedido =localStorage.getItem('totalFinal');
-  let number = numberPizza2.text();
-  number = parseInt(number) - 1;
-  numberPizza2.text(number); // mostrando valores 
-  let precio = $(this).data('precio');
-  let final = parseFloat(totalPedido)-parseFloat(precio);
-  localStorage.setItem('totalFinal', final.toFixed(1));
-  showTotal.text(localStorage.getItem('totalFinal'))
-});
-
-
+let templateProducts = (element, container) => {
+  let template = ` <div class="row">
+  <div class="col-8">${element.nombre}</div>
+  <div class="col-4 row">
+    <div class="col-4">
+      <button class="increment" data-precio=${element.precio}>+</button>
+    </div>
+    <div class="col-4" id=${element.nombre}>0</div>
+    <div class="col-4">
+      <button class="decrement" data-precio=${element.precio}>-</button>
+    </div>
+  </div>
+</div>`;
+  container.append(template);
+}

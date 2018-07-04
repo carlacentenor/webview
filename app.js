4cae564efb6fa9c52cafa2e3e72da3095389a56b
carlacentenor <carlacentenor@gmail.com>
2018-07-04 13:24:13 -0500

    avance algoritmo de venta

--- app.js
+++ app.js
@@ -1,12 +1,51 @@
-const increment = $('.increment');
-const decrement = $('.decrement');
+const increment = $('#increment');
+const decrement = $('#decrement');
 let numberPizza = $('#americanagrande');
+let numberPizza2 = $('#pepperonigrande');
+let totalFinal = 0;
+localStorage.setItem('totalFinal', totalFinal);
+let showTotal = $('#total');
 
-$(document).on('click', '.increment', function() {
-let number =numberPizza.text();
-number = parseInt(number)+1;
-numberPizza.text(number);
-let precio = $(this).data('precio');
-let total = parseInt(number)*precio;
-console.log(total);
-})
+$(document).on('click', '#increment', function () {
+  let totalPedido =localStorage.getItem('totalFinal');
+  let number = numberPizza.text();
+  number = parseInt(number) + 1;
+  numberPizza.text(number); // mostrando valores 
+  let precio = $(this).data('precio');
+  let final =parseFloat(precio) + parseFloat(totalPedido);
+  localStorage.setItem('totalFinal', final.toFixed(1));
+  showTotal.text(localStorage.getItem('totalFinal'))
+});
+
+$(document).on('click', '#decrement', function () {
+  let totalPedido =localStorage.getItem('totalFinal');
+  let number = numberPizza.text();
+  number = parseInt(number) - 1;
+  numberPizza.text(number); // mostrando valores 
+  let precio = $(this).data('precio');
+  let final = parseFloat(totalPedido)-parseFloat(precio);
+  localStorage.setItem('totalFinal', final.toFixed(1));
+  showTotal.text(localStorage.getItem('totalFinal'))
+});
+
+$(document).on('click', '#increment1', function () {
+  let totalPedido =localStorage.getItem('totalFinal');
+  let number = numberPizza2.text();
+  number = parseInt(number) + 1;
+  numberPizza2.text(number); // mostrando valores 
+  let precio = $(this).data('precio');
+  let final =parseFloat(precio) + parseFloat(totalPedido);
+  localStorage.setItem('totalFinal', final.toFixed(1));
+  showTotal.text(localStorage.getItem('totalFinal'))
+});
+
+$(document).on('click', '#decrement2', function () {
+  let totalPedido =localStorage.getItem('totalFinal');
+  let number = numberPizza2.text();
+  number = parseInt(number) - 1;
+  numberPizza2.text(number); // mostrando valores 
+  let precio = $(this).data('precio');
+  let final = parseFloat(totalPedido)-parseFloat(precio);
+  localStorage.setItem('totalFinal', final.toFixed(1));
+  showTotal.text(localStorage.getItem('totalFinal'))
+});
